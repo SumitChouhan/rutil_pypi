57d88ebd0636e40c5a18d7717cfe63c03c68c67f
Sumit Chouhan <sumit.chouhan@relyance.ai>
2021-04-08 21:26:05 +0530

    Test

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='rutil_pypi',
+    version='0.0.1',
+    description='My private package from private github repo',
+    #url='git@github.com:rfschubert/ptolemaios-sdk-package.git',
+    url='git@github.com:SumitChouhan/rutil_pypi.git',
+    author='Sumit Chouhan',
+    author_email='sumit.chouhan19@gmail.com',
+    license='unlicense',
+    packages=['rutil_pypi'],
+    zip_safe=False
+)
+
